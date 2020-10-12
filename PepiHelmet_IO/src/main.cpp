d341e81ffaf4e9a8e6114e5cf587e2aabec5162e
Pentacosiarca <Pentacosiarca@users.noreply.github.com>
2020-10-12 19:55:33 +0200

    Update main.cpp

--- PepiHelmet_IO/src/main.cpp
+++ PepiHelmet_IO/src/main.cpp
@@ -9,9 +9,10 @@
 
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN   1
-#define LED_COUNT 1
+#define LED_COUNT 5
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 
 #define I2C_TIMEOUT 1000
 #define I2C_PULLUP 1
@@ -19,50 +20,61 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 #define SDA_PIN 0
 #define SCL_PORT PORTB
 #define SCL_PIN 2
+
 #include <SoftI2CMaster.h>
 
 #define I2C_7BITADDR (0x19) // with SDO --> Vcc
 #define ADDRLEN 1        // address length, usually 1 or 2 bytes
 
-#include <tools.h>
 int time_d_ms = 10;
 
 const int averageCTE = 10;
 const int dimensions = 3;
+
+long coordArray[dimensions][averageCTE] = {NULL};
 int count = 0;
 
-int* max_vals = NULL;
-int* min_vals = NULL;
+long *ptrCoordArray = &coordArray[0][0];
 
-int* coordArray = create2Darray(dimensions, averageCTE);
+long averageVal[dimensions] = {NULL};
+long minVal[dimensions] = {NULL};
+long maxVal[dimensions] = {NULL};
+
+long *ptrAverage = &averageVal[0];
+long *ptrMin = &minVal[0];
+long *ptrMax = &maxVal[0];
+
+int red,green,blue;
+
+#include "tools.h"
 
 void setup(void) {
+ 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
 
-  #include <setupReg.h>
+  #include "setupReg.h"
 
 }
 
 void loop(void){
-  
-  int* moduleAB;
-  
-  //delay(time_d_ms);
 
-  XYZcoord Coordinates_b;
-  Coordinates_b.ReturnVector(coordArray,dimensions,count);
-  max_vals = Coordinates_b.ReturnMax();
-  min_vals = Coordinates_b.ReturnMin();
+// GET accel data and STORE
+  long *ptrCoordCount = &coordArray[0][count];
+  getXYZcoord(ptrCoordCount, ptrMax, ptrMin);
   
-  count += 1;
-  if (count > 10){
-    count = 0;
+// COMPUTE from array
+  computeFromXYZ(ptrCoordArray, ptrAverage, averageCTE, dimensions);
 
-    POperation Vecoperator(coordArray,dimensions,count);
-    moduleAB = Vecoperator.Module();
+// DETERMINE rgb values
+  performColour(ptrAverage, ptrMin, ptrMax, &red, &green, &blue, dimensions);
+  
+// SEND rgb to neopixel
+  sendColour(red, green, blue);
 
-    Sendcolour Sendcolour(coordArray, max_vals, min_vals, moduleAB, dimensions, averageCTE);
-  }
+
+  count++;
+  if(count>averageCTE-1)count=0;
+
 }
