398368dd4014122489c2d65f02843852d22101eb
Pentacosiarca <Pentacosiarca@users.noreply.github.com>
2020-10-12 19:56:14 +0200

    Create setupReg.h

---
+++ PepiHelmet_IO/src/setupReg.h
@@ -0,0 +1,60 @@
+/*sequence of Registers manipulation from the accdemo example from Adafruit*/
+
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x0F);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_READ);
+  i2c_read(true);
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x20);
+  i2c_write(0x07);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x20);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_READ);
+  i2c_read(true);
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x20);
+  i2c_write(0x77);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x23);
+  i2c_write(0x88);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x22);
+  i2c_write(0x10);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x1F);
+  i2c_write(0x80);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x23);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_READ);
+  i2c_read(true);
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x23);
+  i2c_write(0x98);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_WRITE);
+  i2c_write(0x23);
+  i2c_stop();
+
+  i2c_rep_start((I2C_7BITADDR<<1)|I2C_READ);
+  i2c_read(true);
